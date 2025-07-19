19f4cb9fed1cdb0093b863b5880db7a8fbae81fe
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2025-07-19 15:28:07 +0200

    Add solution for 2015 - day 1 -  part 1.

--- src/2015/day01.c
+++ src/2015/day01.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello from Day 1!\n");
+    FILE *fp = fopen("input/2015-day01.txt", "r");
+    if (!fp) {
+        perror("Failed to open input file");
+        return 1;
+    }
+
+    int floor = 0;
+    int ch;
+
+    while ((ch = fgetc(fp)) != EOF) {
+        if (ch == '(') {
+            floor++;
+        } else if (ch == ')') {
+            floor--;
+        }
+    }
+
+    fclose(fp);
+    printf("Final floor: %d\n", floor);
     return 0;
 }
