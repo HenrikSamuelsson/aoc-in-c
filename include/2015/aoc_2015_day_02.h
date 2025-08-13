4ecb35691a30449bce94c5d4d7653cffe882b681
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2025-08-13 20:23:30 +0200

    Add an header file for AoC 2015 day 2.

---
+++ include/2015/aoc_2015_day_02.h
@@ -0,0 +1,7 @@
+﻿#ifndef AOC_2015_DAY_02_H
+#define AOC_2015_DAY_02_H
+
+int solve_aoc_2015_day_02_part_1(const char *instructions);
+int solve_aoc_2015_day_02_part_2(const char *instructions);
+
+#endif /* AOC_2015_DAY_02_H */
