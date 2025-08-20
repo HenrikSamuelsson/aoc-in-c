78a5ace59d2ad9a00b62ae865e35cdb60d038b78
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2025-08-20 03:27:19 +0200

    Add header for 2015 day 3.

---
+++ include/2015/aoc_2015_day_03.h
@@ -0,0 +1,7 @@
+#ifndef AOC_2015_DAY_03_H
+#define AOC_2015_DAY_03_H
+
+int solve_aoc_2015_day_03_part_1(const char *instructions);
+int solve_aoc_2015_day_03_part_2(const char *instructions);
+
+#endif /* AOC_2015_DAY_03_H */
