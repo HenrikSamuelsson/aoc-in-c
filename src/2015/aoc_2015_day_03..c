b14e38e2df7b90a30bdec59c36b3b75c5d40aebf
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2025-08-23 04:57:25 +0200

    Create function stubs.

---
+++ src/2015/aoc_2015_day_03..c
@@ -0,0 +1,13 @@
+#include "aoc_2015_day_03.h"
+
+int solve_aoc_2015_day_02_part_1(const char *input)
+{
+    // Stub.
+    return -1;
+}
+
+int solve_aoc_2015_day_02_part_2(const char *input)
+{
+    // Stub.
+    return -2;
+}
