d5268f2b9b153dc0dd1fef625edaa1014a0ad495
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2025-09-05 18:08:06 +0200

    Trying to add more tests, not working.

--- include/2015/aoc_2015_day_03.h
+++ include/2015/aoc_2015_day_03.h
@@ -1,7 +1,10 @@
 #ifndef AOC_2015_DAY_03_H
 #define AOC_2015_DAY_03_H
 
+#include <stdbool.h>
+
 int solve_aoc_2015_day_03_part_1(const char *instructions);
 int solve_aoc_2015_day_03_part_2(const char *instructions);
+bool run_day3_tests(void);
 
 #endif /* AOC_2015_DAY_03_H */
